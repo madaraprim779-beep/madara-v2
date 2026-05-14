f92ddcc37ed5c2227bac39d651afad62af5a57e0
madaraprim779-beep <madaraprim779@gmail.com>
2026-05-14 13:28:16 +0000

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,11 @@ const config = {
     SESSION_ID: process.env.SESSION_ID || 'TON_ID_ICI',
     PREFIX: '+',
     OWNER_NAME: 'Madara Solitaire',
-    BOT_NAME: 'MADARA-V2'
+    BOT_NAME: 'MADARA-V2',
+    VIDEO_URL: 'https://d.uguu.se/DMvhxMoO.mp4',
+    AUDIO_URL: 'https://d.uguu.se/FZksewNM.mp3'
 };
 
 export default config;
+
 
